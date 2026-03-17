4cb2adca1cb6394082e106a875d9454a0e280ef2
Rasil Zargishiev <rizargishiev@edu.hse.ru>
2026-03-17 13:08:42 +0300

    add feature1, feature2, simple_connection

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"nilchanpub/feature1"
+	"nilchanpub/feature_postgres/simple_connection"
+)
 
+func main() {
+	feature1.Feature1()
+	feature1.Feature1()
+
+	simple_connection.CheckConnection()
+	fmt.Println("main end.")
 }
