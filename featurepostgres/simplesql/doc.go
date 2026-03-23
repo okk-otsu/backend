951ea516fef342537174f7a3418e637fafd0ee21
Rasil Zargishiev <rizargishiev@edu.hse.ru>
2026-03-23 17:46:56 +0300

    modified go.doc

--- featurepostgres/simplesql/doc.go
+++ featurepostgres/simplesql/doc.go
@@ -1,2 +1,5 @@
-// Package simplesql ...
+// Package simplesql contains simple PostgreSQL CRUD examples using pgx.
+//
+// It is intended for learning basic SQL operations in Go: table creation,
+// insertion, selection, update, and deletion.
 package simplesql
